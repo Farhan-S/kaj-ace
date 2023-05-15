f055a98f0cc5fbf793c53eadde6c1a1221d08638
mdnuruzzamanemon <mdnuruzzamanemon@gmail.com>
2023-05-15 23:32:39 +0600

    Our own buisyness

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$('.slider').owlCarousel({
+    items        :  1 ,
+    autoPlay     : true ,
+    navigation   : true,
+    navigationText: ["<i class='fa fa-angle-left bam'></i>","<i class='fa fa-angle-right dan'></i>"],
+    pagination   : false,
+});
