7cade592a1217841fb44003b472fbbf9e6d93f86
mdnuruzzamanemon <mdnuruzzamanemon@gmail.com>
2023-05-16 22:26:56 +0600

    change korci

--- js/index.js
+++
@@ -1,7 +0,0 @@
-$('.slider').owlCarousel({
-    items        :  1 ,
-    autoPlay     : true ,
-    navigation   : true,
-    navigationText: ["<i class='fa fa-angle-left bam'></i>","<i class='fa fa-angle-right dan'></i>"],
-    pagination   : false,
-});
